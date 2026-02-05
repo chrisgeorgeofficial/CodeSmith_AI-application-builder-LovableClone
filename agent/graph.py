09776af3804995d3428b121d76e45529a3d48516
CHRIS GEORGE <Chrisgeorge2k20@gmail.com>
2026-02-05 17:07:48 +0530

    added planner prompt

--- agent/graph.py
+++ agent/graph.py
@@ -1,7 +1,35 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from langchain_groq import ChatGroq
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
-resp = llm.invoke("Who invented kriya yoga. answer in one sentence.")
+from pydantic import BaseModel
 
-print(resp.content)
+user_prompt = "create a simple calculator web application"
+
+prompt = f"""
+You are the PLANNER agent. Convert the user prompt into a COMPLETE engineering project plan 
+
+User request: {user_prompt}
+"""
+
+
+
+
+
+
+
+
+
+class Schema(BaseModel):
+    price: float
+    eps: float
+
+resp = llm.with_structured_output(Schema).invoke("Extracct Price and EPS from this report:"
+                                           "NVIDIA reported quaterly EPS of 2.3 and"
+                                           "their current price is $100.")
+
+print(resp)
